f15decf4da0dd5ec64a8513c18a377b629dacb0c
Jesse Min Zhang <jessez@stanford.edu>
2016-05-19 13:37:32 -0700

    added custom cells

--- jz-rnn-tensorflow/model.py
+++ jz-rnn-tensorflow/model.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
-from tensorflow.models.rnn import rnn_cell
+from tensorflow.models.rnn import rnn_cell,rnn
 from tensorflow.models.rnn import seq2seq
+from jz_rnn_cell import *
 
 import numpy as np
 
@@ -11,48 +12,57 @@ class Model():
             args.batch_size = 1
             args.seq_length = 1
 
-        if args.model == 'rnn':
-            cell_fn = rnn_cell.BasicRNNCell
-        elif args.model == 'gru':
-            cell_fn = rnn_cell.GRUCell
-        elif args.model == 'lstm':
-            cell_fn = rnn_cell.BasicLSTMCell
-        else:
-            raise Exception("model type not supported: {}".format(args.model))
+        if args.model == 'rnn': cell_fn = jzRNNCell
+        elif args.model == 'gru': cell_fn = jzGRUCell
+        elif args.model == 'lstm': cell_fn = jzLSTMCell
+        else: raise Exception("model type not supported: {}".format(args.model))
 
-        if args.activation == 'tanh':
-            cell_af = tanh
-        elif args.activation == 'sigmoid':
-            cell_af = sigmoid
-        elif args.activation == 'relu':
-            cell_af = tf.nn.relu
-        else:
-            raise Exception("activation function not supported: {}".format(args.activation))
-
-        cell = cell_fn(args.rnn_size,activation=cell_af)
-
-        self.cell = cell = rnn_cell.MultiRNNCell([cell] * args.num_layers)
+        if args.activation == 'tanh': cell_af = tf.tanh
+        elif args.activation == 'sigmoid': cell_af = tf.sigmoid
+        elif args.activation == 'relu': cell_af = tf.nn.relu
+        else: raise Exception("activation function not supported: {}".format(args.activation))
 
         self.input_data = tf.placeholder(tf.int32, [args.batch_size, args.seq_length])
         self.targets = tf.placeholder(tf.int32, [args.batch_size, args.seq_length])
-        self.initial_state = cell.zero_state(args.batch_size, tf.float32)
 
         with tf.variable_scope('rnnlm'):
-            softmax_w = tf.get_variable("softmax_w", [args.rnn_size, args.vocab_size])
+            if not args.bidirectional:
+                softmax_w = tf.get_variable("softmax_w", [args.rnn_size, args.vocab_size])
+            else:
+                softmax_w = tf.get_variable("softmax_w", [args.rnn_size*2, args.vocab_size])
             softmax_b = tf.get_variable("softmax_b", [args.vocab_size])
             with tf.device("/cpu:0"):
                 embedding = tf.get_variable("embedding", [args.vocab_size, args.rnn_size])
                 inputs = tf.split(1, args.seq_length, tf.nn.embedding_lookup(embedding, self.input_data))
-                inputs = [tf.squeeze(input_, [1]) for input_ in inputs]
+                inputs = [tf.nn.dropout(tf.squeeze(input_, [1]),args.dropout) for input_ in inputs]
 
-        def loop(prev, _):
-            prev = tf.matmul(prev, softmax_w) + softmax_b
-            prev_symbol = tf.stop_gradient(tf.argmax(prev, 1))
-            return tf.nn.embedding_lookup(embedding, prev_symbol)
+        # one-directional RNN (nothing changed here..)
+        if not args.bidirectional:
+            cell = cell_fn(args.rnn_size,activation=cell_af)
+            self.cell = cell = rnn_cell.MultiRNNCell([cell] * args.num_layers)
+            self.initial_state = cell.zero_state(args.batch_size, tf.float32)
+            def loop(prev, _):
+                prev = tf.matmul(prev, softmax_w) + softmax_b
+                prev_symbol = tf.stop_gradient(tf.argmax(prev, 1))
+                return tf.nn.embedding_lookup(embedding, prev_symbol)
+            outputs, last_state = seq2seq.rnn_decoder(inputs, self.initial_state, cell, loop_function=loop if infer else None, scope='rnnlm')
+            output = tf.reshape(tf.concat(1, outputs), [-1, args.rnn_size])
 
-        outputs, last_state = seq2seq.rnn_decoder(inputs, self.initial_state, cell, loop_function=loop if infer else None, scope='rnnlm')
-        output = tf.reshape(tf.concat(1, outputs), [-1, args.rnn_size])
-        self.logits = tf.matmul(output, softmax_w) + softmax_b
+        # bi-directional RNN
+        else:
+            lstm_fw = cell_fn(args.rnn_size,activation=cell_af)
+            lstm_bw = cell_fn(args.rnn_size,activation=cell_af)
+            self.lstm_fw = lstm_fw = rnn_cell.MultiRNNCell([lstm_fw]*args.num_layers)
+            self.lstm_bw = lstm_bw = rnn_cell.MultiRNNCell([lstm_bw]*args.num_layers)
+            self.initial_state_fw = lstm_fw.zero_state(args.batch_size,tf.float32)
+            self.initial_state_bw = lstm_bw.zero_state(args.batch_size,tf.float32)
+            outputs,_,_ = rnn.bidirectional_rnn(lstm_fw, lstm_bw, inputs,
+                                            initial_state_fw=self.initial_state_fw,
+                                            initial_state_bw=self.initial_state_bw,
+                                            sequence_length=args.seq_length) 
+            output = tf.reshape(tf.concat(1, outputs), [-1, args.rnn_size*2])
+
+        self.logits = tf.matmul(tf.nn.dropout(output,args.dropout), softmax_w) + softmax_b
         self.probs = tf.nn.softmax(self.logits)
         loss = seq2seq.sequence_loss_by_example([self.logits],
                 [tf.reshape(self.targets, [-1])],
@@ -103,5 +113,3 @@ class Model():
             ret += pred
             char = pred
         return ret
-
-
